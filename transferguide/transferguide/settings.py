d09df85b2f5024bb21bb1c8c599f777746ba3637
newgazar1 <72676296+newgazar1@users.noreply.github.com>
2023-02-12 14:22:15 -0500

    testing setting2

--- transferguide/transferguide/settings.py
+++ transferguide/transferguide/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-w2%x3gbocktx6f#-f&mfe-+gadr1##67z--rxv#94n!(vn#5wd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','staff-build-example.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','project-b-26.herokuapp.com']
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
