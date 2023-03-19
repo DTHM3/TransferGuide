d3d115b6e1997987c94c451e5c70e3936170a2b4
Jdaddy21 <suhjeremy@gmail.com>
2023-03-19 17:41:12 -0400

    name change

--- transferguideApp/models.py
+++ transferguideApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class MyClass(models.Model):
+class UVAClass(models.Model):
     class_id = models.CharField(max_length=50)
     subject = models.CharField(max_length=50)
     class_description = models.TextField()
