61e7fd9d6345d58f9dc96527379ba568ee885a61
Jdaddy21 <suhjeremy@gmail.com>
2023-03-19 17:40:50 -0400

    class model

--- transferguideApp/models.py
+++ transferguideApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class MyClass(models.Model):
+    class_id = models.CharField(max_length=50)
+    subject = models.CharField(max_length=50)
+    class_description = models.TextField()
+    instructors = models.JSONField()
+    units = models.IntegerField()
