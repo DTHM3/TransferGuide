31c5db380d78932e4cb0aca9a654b8c08fc411f8
jdkang1 <113106630+jdkang1@users.noreply.github.com>
2023-03-20 16:55:42 -0400

    Update tests.py

--- transferguideApp/tests.py
+++ transferguideApp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import UVAClass
+
+
+class ClassSearchTest(TestCase):
+    results = []
+
+    def setUp(self):
+        self.results.append(UVAClass.objects.create(class_id="test", subject="test subject",
+                                                    class_description="test desc",
+                                                    instructors={'name': "test name", "email": "test email"}, units=3))
+
+    def test_search_success(self):
+        response = self.client.get("/guide/search/")
+        self.assertContains(response, "id")
