c5b808723b48a665e798104e3f2fd633050f9279
Jdaddy21 <suhjeremy@gmail.com>
2023-03-19 22:08:26 -0400

    load and search

--- transferguide/urls.py
+++ transferguide/urls.py
@@ -20,15 +20,16 @@ from django.contrib import admin
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path, include
 from django.views.generic import TemplateView
+from .query_view import render_template
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("admin-account-login/",TemplateView.as_view(template_name="admin.html")),
 
-    path('', TemplateView.as_view(template_name="index.html")),
+    # path('', TemplateView.as_view(template_name="index.html")),
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view()),
-
-
+    path('', render_template),
+    
     path('guide/', include('transferguideApp.urls')),
 ]
